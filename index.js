1e5d4742c1520b9f18e8db0372ae4246f0a8f985
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-04-26 17:11:39 -0500

    Installed bitcoinavergare npm directly
    Installed bitcoinavergare npm directly into the project instead of calling the API

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const bodyParser = require("body-parser");
 const request = require ("request");
 const app = express();
 
+//bitcoinaverage
+const ba = require('bitcoinaverage');
+
+var publicKey = 'YzA4NmFkNWM5ODUyNGMwZWE1Njc3OTNhY2M3Mjg2ZDQ';
+var secretKey = 'YTBmOGQ2MjQ2YzBmNDUwYjk0MWE2ZTdhNDI2NjkwYzAxN2Q1YWZhMjc1MTc0NjljOTc5N2M0MzA2NjU4NWFmYw';
+
+var restClient = ba.restfulClient(publicKey, secretKey);
+var wsClient = ba.websocketClient(publicKey, secretKey);
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
@@ -16,32 +25,49 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
 // console.log(req.body.crypto);
 // console.log(req.body.fiat);
-var crypto = req.body.crypto;
-var fiat = req.body.fiat;
-var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-finalURL = baseURL + crypto + fiat;
-request ( "https://blockchain.info/ticker", function(error, response, body){
-  console.log("*****************************");
-  // console.log(body);
 
-  var data = JSON.parse(body);
-    var price = data.USD.last;
-  // var dailyAverage = data.averages.day;
-  // var weeklyAverage = data.averages.week;
+var symbol_set = 'global';
+var symbol = 'BTCUSD';
 
-  console.log("price " + data);
-  console.log("*****************************");
-  // console.log("Daily average " + dailyAverage);
-  console.log("*****************************");
-  // console.log("Weekly average " + weeklyAverage);
+restClient.getTickerDataPerSymbol('global', 'BTCUSD', function(response) {
 
-  // currentDate = data.display_timestamp;
-  // res.write("<h1>The current time is " +currentDate +"</h1>");
-  res.write("<h1>The current Bitcoin Price is " + price + "</h1>");
-  res.send();
-});
+   var data = JSON.parse(response);
+   var price = data.last;
+    console.log(price);
+
+
+}, function(error){
+    console.log(error);
+}) ;
+
+// var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+// finalURL = baseURL + crypto + fiat;
+// request ( finalURL, function(error, response, body){
+//   console.log("*****************************");
+//
+
+//   // console.log(body);
+//
+//   // var data = JSON.parse(body);
+//      // var price = response.last;
+//   // var dailyAverage = data.averages.day;
+//   // var weeklyAverage = data.averages.week;
+//   //
+//   // console.log("price " + price);
+//   // console.log("*****************************");
+//   // console.log("Daily average " + dailyAverage);
+//   // console.log("*****************************");
+//   // console.log("Weekly average " + weeklyAverage);
+//   //
+//   // currentDate = data.display_timestamp;
+//   // res.write("<h1>The current time is " +currentDate +"</h1>");
+//   // res.write("<h1>The current Bitcoin Price is " + price + "</h1>");
+//   // res.send();
+// });
 
 
 })
